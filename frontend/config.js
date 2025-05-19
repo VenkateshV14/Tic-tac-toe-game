871b9b90c9cfaac77d35398c8ee503fedb537925
VenkateshV14 <vakamudilavenkatesh@gmail.com>
2025-05-19 18:47:59 +0530

    project updated

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,7 @@
 const config = {
-  BOT: 'http://bot-service:8000',
-  LANG: 'http://lang-service:8001',
-  DB: 'http://db:8002'
+  BOT: 'http://a84c0138167404df5a0829ad5910f3f8-1910648360.us-east-1.elb.amazonaws.com:8000',
+  LANG: 'http://a52068e5a8a06415d93a638acdd7215e-2054956178.us-east-1.elb.amazonaws.com:8001',
+  DB: 'http://a713eaaea68524915aea150d33236020-587768316.us-east-1.elb.amazonaws.com:8002'
 };
 
 export default config;
